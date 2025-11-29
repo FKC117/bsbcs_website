d5ae5c701d9eae650844ca03058aa469239deb44
FKC 117 <fazlul.karim117@gmail.com>
2025-11-29 16:09:11 +0600

    Hero content of all the pages are done

--- bsbcs/website/templatetags/site_tags.py
+++ bsbcs/website/templatetags/site_tags.py
@@ -2,7 +2,7 @@ from django import template
 from django.urls import reverse, NoReverseMatch
 from django.core.cache import cache
 
-from website.models import SiteSettings, NavigationLink
+from website.models import SiteSettings, NavigationLink, HeroSection, CallToAction
 
 register = template.Library()
 
@@ -47,3 +47,35 @@ def get_navigation_links():
     
     cache.set('navigation_links', navs, CACHE_TIMEOUT)
     return navs
+
+
+@register.simple_tag
+def get_hero_section(page_name):
+    """Return the HeroSection instance for a specific page.
+    
+    Results are cached for 1 hour to reduce database queries.
+    """
+    cache_key = f'hero_section_{page_name}'
+    cached = cache.get(cache_key)
+    if cached is not None:
+        return cached
+    
+    hero = HeroSection.objects.filter(page=page_name).first()
+    cache.set(cache_key, hero, CACHE_TIMEOUT)
+    return hero
+
+
+@register.simple_tag
+def get_call_to_action(page_name):
+    """Return the CallToAction instance for a specific page.
+    
+    Results are cached for 1 hour to reduce database queries.
+    """
+    cache_key = f'call_to_action_{page_name}'
+    cached = cache.get(cache_key)
+    if cached is not None:
+        return cached
+    
+    cta = CallToAction.objects.filter(page=page_name).first()
+    cache.set(cache_key, cta, CACHE_TIMEOUT)
+    return cta
